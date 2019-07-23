933977624fa25a483e832d7c3abe937f8f3a54fa
Victor <victorcr1@icloud.com>
2019-07-23 10:35:28 +0200

    finished part 5 of the tutorial (testing)

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -39,6 +39,15 @@ class QuestionModelTests(TestCase):
 		self.assertIs(recent_question.was_published_recently(), True) 
 
 
+
+
+
+
+
+
+
+
+
 def create_question(question_text, days):
 	"""
 	Create a question with a specific question_text and published 
@@ -55,7 +64,7 @@ class QuestionIndexViewTests(TestCase):
 		"""
 		If no questions exist, an appropiate message is displayed.
 		"""
-		response = self.client.get(reverser('polls:index'))
+		response = self.client.get(reverse('polls:index'))
 		self.assertEqual(response.status_code, 200)
 		self.assertContains(response, "No polls are available.")
 		self.assertQuerysetEqual(response.context['latest_question_list'], [])
@@ -65,7 +74,7 @@ class QuestionIndexViewTests(TestCase):
 		Questions with a pub_date in the past are displayed on 
 		the index page.
 		"""
-		create_question(questions_text="Past question.", days=-30)
+		create_question(question_text="Past question.", days=-30)
 		response = self.client.get(reverse('polls:index'))
 		self.assertQuerysetEqual(
 			response.context['latest_question_list'],
@@ -87,8 +96,8 @@ class QuestionIndexViewTests(TestCase):
 		Even if both past and future questions exist, display only on the 
 		index page the past questions.
 		"""
-		create_question(question_text="Past Question.", days=-30)
-		create_question(question_text="Future Question.", days=30)
+		create_question(question_text="Past question.", days=-30)
+		create_question(question_text="Future question.", days=30)
 		response = self.client.get(reverse('polls:index'))
 		self.assertQuerysetEqual(
 			response.context['latest_question_list'],
@@ -96,14 +105,38 @@ class QuestionIndexViewTests(TestCase):
 			)
 
 	def test_two_past_questions(self):
-		create_question(question_text="Past Question 1.", days=-30)
-		create_question(question_text="Past Question 2.", days=-5)
+		create_question(question_text="Past question 1.", days=-30)
+		create_question(question_text="Past question 2.", days=-5)
 		response = self.client.get(reverse('polls:index'))
 		self.assertQuerysetEqual(
 			response.context['latest_question_list'],
 			['<Question: Past question 2.>', '<Question: Past question 1.>']
 			)
 
+
+
+
+
+
+
+class QuestionDetailViewTests(TestCase):
+	def test_future_question(self):
+		"""
+		The detail view of a question in the future returns a 404 not found. 
+		"""
+		future_question = create_question(question_text="Future question.", days=30)
+		url = reverse('polls:detail', args=(future_question.id,))
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, 404)
+
+	def test_past_question(self):
+		"""
+		The detail view for a question in the past displays the question_text.
+		"""
+		past_question = create_question(question_text="Past question.", days=-30)
+		url = reverse('polls:detail', args=(past_question.id,))
+		response = self.client.get(url)
+		self.assertContains(response, past_question.question_text)
 
 
 
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -26,42 +26,23 @@ class IndexView(generic.ListView):
 			).order_by('-pub_date')[:5]
 
 
+
+
 class DetailView(generic.DetailView):
 	model = Question
 	template_name = 'polls/detail.html'
+	def get_queryset(self):
+		"""
+		Excludes any questions that aren't published yet.
+		"""
+		return Question.objects.filter(pub_date__lte=timezone.now())
+
+
 
 
 class ResultsView(generic.DetailView):
 	model = Question
 	template_name = 'polls/results.html'
-
-
-
-
-# def index(request):
-# 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	
-# 	context = {
-# 		'latest_question_list': latest_question_list,
-# 	}
-# 	return render(request, 'polls/index.html', context)
-
-# def detail(request, question_id):
-# 	"""try: 
-# 		question = Question.objects.get(pk=question_id)
-# 	except Question.DoesNotExist:
-# 		raise Http404("Question does not exist")
-# 	return render(request, 'polls/detail.html', {'question': question})"""
-# 	question = get_object_or_404(Question, pk=question_id)
-# 	return render(request, 'polls/detail.html', {'question': question})
-	
-# def results(request, question_id):
-# 	question = get_object_or_404(Question, pk=question_id)
-# 	return render(request, 'polls/results.html', {'question': question})
-
-
-
-
 
 
 
@@ -83,3 +64,27 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
 	return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+
+
+	# def index(request):
+# 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	
+# 	context = {
+# 		'latest_question_list': latest_question_list,
+# 	}
+# 	return render(request, 'polls/index.html', context)
+
+# def detail(request, question_id):
+# 	"""try: 
+# 		question = Question.objects.get(pk=question_id)
+# 	except Question.DoesNotExist:
+# 		raise Http404("Question does not exist")
+# 	return render(request, 'polls/detail.html', {'question': question})"""
+# 	question = get_object_or_404(Question, pk=question_id)
+# 	return render(request, 'polls/detail.html', {'question': question})
+	
+# def results(request, question_id):
+# 	question = get_object_or_404(Question, pk=question_id)
+# 	return render(request, 'polls/results.html', {'question': question})
